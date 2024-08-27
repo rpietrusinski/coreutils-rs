052939abf0a3456742c65deb67b11bab00f0485d
Robert Pietrusinski <robert.x.pietrusinski@gsk.com>
2024-08-27 15:11:02 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # Coreutils-rs
+//!
+//! `Coreutils-rs` is a collection of utilities reproducing some of GNU coreutils packages.
+//! The project aims purely for Rust programming learning purposes.
+
 pub mod config;
 pub mod runner;
 pub mod search;
